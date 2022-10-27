7225e5896a7752c3d2a8b32d7987c1dd085fbef1
Andrei Barboni <andrei.barboni01@e-uvt.ro>
2022-10-27 13:57:35 +0300

    Lab4 commit

--- src/main/java/ImageProxy.java
+++ src/main/java/ImageProxy.java
@@ -8,7 +8,7 @@ public class ImageProxy implements Element, Picture {
         this.url = url;
     }
 
-    Image loadImage(){
+    public Image loadImage(){
         if(realImg == null){
             realImg = new Image(url);
         }
